ef866ef714c04907948760eea7db9da365f0fb61
JEET JD DAS <JEET JD DAS@DESKTOP-K57E8BB>
2020-06-26 23:39:29 +0530

    Test Engineer 2 Develope the code

---
+++ RAS/src/test/java/com/ras/testscript/TestFramework2.java
@@ -0,0 +1,14 @@
+package com.ras.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestFramework2 
+{
+	@Test
+	public void tc_03()
+	{
+		Reporter.log("Test Engineer Develope this method from other system",true);
+	}
+	
+}
